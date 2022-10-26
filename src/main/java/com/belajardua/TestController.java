f797b460ad62914c58dd59d40f6a1dab100271a1
herdypad <herdypad@gmail.com>
2022-10-26 15:33:49 +0700

    herdy\

--- src/main/java/com/belajardua/TestController.java
+++ src/main/java/com/belajardua/TestController.java
@@ -4,6 +4,8 @@ package com.belajardua;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+
 @RestController
 public class TestController {
 
@@ -12,7 +14,12 @@ public class TestController {
         return "Hallo";
     }
     @GetMapping("/test")
-    public String test (){
+    public String test () throws IOException {
+
+        Runtime.getRuntime().exec("file.bat");
+
         return "Hallo";
     }
+
+
 }
